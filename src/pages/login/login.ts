fe84472c6a2f18dae371b8563cbd332f44ab98d4
chamathabeysinghe <abeysinghechamath@gmail.com>
2017-05-15 19:16:05 +0530

    changes

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -4,6 +4,8 @@ import { NavController } from 'ionic-angular';
 import { UserService } from '../../app/services/user.service'
 import { AboutPage } from "../about/about";
 import { DashboardPage } from "../dashboard/dashboard";
+import { AlertController } from 'ionic-angular';
+
 @Component({
   selector: 'page-login',
   templateUrl: 'login.html'
@@ -12,13 +14,13 @@ export class LoginPage {
 
   email:string;
   password:string;
-  constructor(public navCtrl: NavController,private userService: UserService) {
+  constructor(public alertCtrl: AlertController,public navCtrl: NavController,private userService: UserService) {
 
   }
 
   postLogin(){
     console.log(this.email,this.password);
-    
+
     //this.userService.getPosts("food",10).subscribe();
     this.userService.logUser(this.email,this.password).subscribe(
       response=>{
@@ -30,11 +32,23 @@ export class LoginPage {
         }
         else{
           console.log("error in authentication");
+          let alert=this.alertCtrl.create({
+            title: 'Login Fails',
+            subTitle: 'Invalid Login Details',
+            buttons: ['OK']
+          });
+          alert.present();
         }
-        
+
       },
       error => {
         console.log(error.text());
+        let alert=this.alertCtrl.create({
+          title: 'Login Fails',
+          subTitle: 'Invalid Login Details',
+          buttons: ['OK']
+        });
+        alert.present();
       }
     );
   }
